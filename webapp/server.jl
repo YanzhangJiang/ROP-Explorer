72288e0966c3ee4a3910dbf22ab64eacf075c485
Yanzhang <resicojyz@gmail.com>
2026-03-07 00:45:04 +0800

    fix server bug

--- webapp/server.jl
+++ webapp/server.jl
@@ -1,5 +1,11 @@
 #!/usr/bin/env julia
 
+import Pkg
+
+# Make the server resilient to how it is launched.
+# This allows `julia webapp/server.jl` as well as `--project=webapp`.
+Pkg.activate(@__DIR__; io=devnull)
+
 using ROPExplorerBackend
 
 ROPExplorerBackend.main()
